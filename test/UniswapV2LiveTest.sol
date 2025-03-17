a7df734a75fd471433b9303e668d864ab2db3745
sinycat <sinycat@163.com>
2025-03-17 13:12:41 +0800

    add MyDex.sol

--- test/UniswapV2LiveTest.sol
+++ test/UniswapV2LiveTest.sol
@@ -13,11 +13,11 @@ import "../src/interfaces/IUniswapV2Pair.sol";
 
 contract UniswapV2LiveTest is Test {
     // Use deployed contract addresses
-    address constant WETH_ADDRESS = 0xa513E6E4b8f2a923D98304ec87F64353C4D5C853;
-    address constant TOKEN_A_ADDRESS = 0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6;
-    address constant TOKEN_B_ADDRESS = 0x8A791620dd6260079BF849Dc5567aDC3F2FdC318;
-    address constant FACTORY_ADDRESS = 0xA51c1fc2f0D1a1b8494Ed1FE312d7C3a78Ed91C0;
-    address constant ROUTER_ADDRESS = 0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82;
+    address constant WETH_ADDRESS = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
+    address constant TOKEN_A_ADDRESS = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512;
+    address constant TOKEN_B_ADDRESS = 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0;
+    address constant FACTORY_ADDRESS = 0x5FC8d32690cc91D4c39d9d3abcBD16989F875707;
+    address constant ROUTER_ADDRESS = 0x0165878A594ca255338adfa4d48449f69242Eb8F;
     
     UniswapV2Factory public factory;
     UniswapV2Router public router;
